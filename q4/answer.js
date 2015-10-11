cfe3edaee2365c400833cbb6d81310a9a065ef1e
Maciej Kurek <maciejk77@gmail.com>
2015-10-11 14:18:46 +0100

    Q4 completed - final

--- q4/answer.js
+++ q4/answer.js
@@ -1,6 +1,9 @@
 var arr = [];
 var arrNew = [];
 
+// Function to return combination of numbers X & Y as per below comment
+// [2, 2, 2, 3, 2, 4, 3, 2, 3, 3, 3, 4, 4, 2, 4, 3, 4, 4]
+
 function getCombinations(numberX, numberY) {
   var diff = numberY - numberX;
   for(var x = 0; x < diff + 1; x++) {
@@ -15,33 +18,32 @@ function getCombinations(numberX, numberY) {
 getValues();
 };
 
+// Function to return array of values as per below comment
+// [4, 8, 16, 9, 27, 81, 16, 64, 256]
 
-var arrNew = [];
-var arr = [2, 2, 2, 3, 2, 4, 3, 2, 3, 3, 3, 4, 4, 2, 4, 3, 4, 4];
-
-// function getValues() {
+function getValues() {
   for(var i = 0; i < arr.length; i += 2) {
-    //console.log(i);
     var el = Math.pow(arr[i], arr[i + 1]);
-    console.log(el);
-   // arr.splice([0], 2);
-   //console.log(arr);
     arrNew.push(el);
-    console.log(arrNew); 
   }
+  // Function to sort results to get array as per below comment
+  // [4, 8, 9, 16, 16, 27, 64, 81, 256]
   arrNew.sort(function(a, b) {return a - b});
   getDistinct();
 }
 
-var arrNew = [4, 8, 9, 16, 16, 27, 64, 81, 256];
+// Function to only get distinct numbers in an array
+// [4, 8, 9, 16, 27, 64, 81, 256]
 
-//function getDistinct() {
+function getDistinct() {
   for(var i = 0; i < arrNew.length; i++) {
     if(arrNew[i] === arrNew[i + 1]) {
       arrNew.splice(i, 1);
     }
   }
-arrNew;
+// Explicitly returns 8 - number of distinct elements in the final array arrNew
+return arrNew.length;
 }
 
+//Call to execute a sequence of functions, providing lower and upper bounds as arguments
 getCombinations(2, 4);
