eaf7dcb55c1c606f41d90da3a1e4b07760983210
Maciej Kurek <maciejk77@gmail.com>
2015-10-11 12:06:49 +0100

    Q1 & Q4 array with combinations completed

--- q4/answer.js
+++ q4/answer.js
@@ -1,10 +1,18 @@
-var sampleNumbers = [2,4];
 var arr = [];
 
-function getVariations(sampleNumbers) {
-  var diff = sampleNumbers[1] - sampleNumbers[0];
-  for(var i = 0; i < diff; i++) {
-    var value = sampleNumbers[0] * sampleNumbers[0+i];
-    arr.push(value);
+function getCombinations(numberX, numberY) {
+  var diff = numberY - numberX;
+  for(var x = 0; x < diff + 1; x++) {
+    var elx = numberX + x
+    
+    for(var y = 0; y < diff + 1; y++) {
+      var ely = numberX + y
+      arr.push(elx)
+      arr.push(ely)
+      console.log(elx);
+      console.log(ely);
+    }
   }
+// console.log(arr);
 };
+// getCombinations(2, 4);
