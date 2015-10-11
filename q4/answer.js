5e1171dd4226ce3a6d94b52715ec6027add1036c
Maciej Kurek <maciejk77@gmail.com>
2015-10-11 13:37:23 +0100

    Q4 with comments

--- q4/answer.js
+++ q4/answer.js
@@ -1,4 +1,5 @@
 var arr = [];
+var arrNew = [];
 
 function getCombinations(numberX, numberY) {
   var diff = numberY - numberX;
@@ -9,10 +10,29 @@ function getCombinations(numberX, numberY) {
       var ely = numberX + y
       arr.push(elx)
       arr.push(ely)
-      console.log(elx);
-      console.log(ely);
+      // console.log(elx);
+      // console.log(ely);
     }
   }
-// console.log(arr);
+getValues(arr);
 };
+
+
+var arrNew = [];
+var arr = [2, 2, 2, 3, 2, 4, 3, 2, 3, 3, 3, 4, 4, 2, 4, 3, 4, 4];
+
+// function getValues() {
+  for(var i = 0; i < arr.length; i = i + 2) {
+    console.log(i);
+    var el = Math.pow(arr[i], arr[i + 1]);
+    console.log(el);
+   // arr.splice([0], 2);
+    console.log(arr);
+    arrNew.push(el);
+    console.log(arrNew); 
+  }
+  // arrNew.sort();
+// }
+// getValues();
+
 // getCombinations(2, 4);
